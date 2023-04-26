d819a29e96b26d91b9891a940793d387102986c4
LittleCanOpener <hrafn8@gmail.com>
2023-04-26 13:28:59 +0000

    reversed alot of test code

--- script.js
+++ script.js
@@ -3,32 +3,34 @@
     // window.localStorage.remove("example-title");
 
 
-// Submit Button & Local Storage
+//  Local Storage
 let libary = [];
-// Storing IP Address in Local Storage
+
+//Storing IP Address in Local Storage
 function addBook () {
     let book = {
         title: document.getElementById("title").value,
         author: document.getElementById("author").value,
-        pages: document.getElementById("pages").value
+        pages: document.getElementById("pages").value,
     }
     libary.push(book);
     document.querySelector("form").reset();
     sessionStorage.setItem('MyLibary', JSON.stringify(libary) );
 }
 
-// Creating Elements
+// Creating figure
 function createFigure() {
     const body = document.getElementById("body");
-    const figure = document.createElement("figure");
     // Creating Figure & Figcaption
-    figure.setAttribute("id", "fig");
+    const figure = document.createElement("figure");
+    figure.setAttribute("id", "outer-container-img");
     body.appendChild(figure);
-
     const figcaption = document.createElement("figcaption");
     figure.appendChild(figcaption);
+    figcaption.setAttribute("id", "container-img");
 }
 
+// Main Event
 //el would be the element that received the click event. ev is the actual click event itself
 document.addEventListener("submit", (ev) => {
     ev.preventDefault();
