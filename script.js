30e8914aae21fa9495e96e95ba262d7f09da8194
LittleCanOpener <hrafn8@gmail.com>
2023-04-26 14:03:12 +0000

    found out how to display sessionStorage

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function addBook () {
     let book = {
         title: document.getElementById("title").value,
         author: document.getElementById("author").value,
-        pages: document.getElementById("pages").value,
+        pages: document.getElementById("pages").value
     }
     libary.push(book);
     document.querySelector("form").reset();
@@ -20,21 +20,25 @@ function addBook () {
 
 // Creating figure
 function createFigure() {
-    const body = document.getElementById("body");
     // Creating Figure & Figcaption
+    const body = document.getElementById("body");
     const figure = document.createElement("figure");
     figure.setAttribute("id", "outer-container-img");
     body.appendChild(figure);
+
     const figcaption = document.createElement("figcaption");
-    figure.appendChild(figcaption);
     figcaption.setAttribute("id", "container-img");
+    figure.appendChild(figcaption);
 }
 
 // Main Event
 //el would be the element that received the click event. ev is the actual click event itself
 document.addEventListener("submit", (ev) => {
     ev.preventDefault();
+
     createFigure()
     addBook()
 
 });
+
+// document.getElementById('outer-container-img').innerHTML = sessionStorage.getItem('MyLibary')
