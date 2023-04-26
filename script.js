9a77e5ba1b43e5c64a4943e548e78fe8d906aa52
LittleCanOpener <hrafn8@gmail.com>
2023-04-26 11:55:26 +0000

    added comments to the js file

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-
-// const form = document.querySelector("form");
-// const form = document.getElementById("form");
-// const title = document.getElementById("title");
-// const author = document.getElementById("author");
-// const pages = document.getElementById("pages");
-
-// Submit Button & Local Storage
-document.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const newTitleValue = title.value;
-    const newNameValue = author.value;
-    const newNumberValue = pages.value;
-
-    window.sessionStorage.setItem("title", newTitleValue);
-    window.sessionStorage.setItem("author", newNameValue);
-    window.sessionStorage.setItem("pages", newNumberValue);
-
-    createFigure()
-
+    // window.sessionStorage.setItem
     // window.localStorage.clear();
     // window.localStorage.remove("example-title");
-});
 
+
+// Submit Button & Local Storage
+let libary = [];
+//el would be the element that received the click event. ev is the actual click event itself
+const addBook = (ev)=>{
+    ev.preventDefault();
+    let book = {
+        id: Date.now(),
+        title: document.getElementById("title").value,
+        author: document.getElementById("author").value,
+        pages: document.getElementById("pages").value
+    }
+    libary.push(book);
+    document.querySelector("form").reset();
+    localStorage.setItem("MyLibary", JSON.stringify(libary) );
+}
+
+// Creating Elements
 function createFigure() {
     const body = document.getElementById("body")
     const figure = document.createElement("figure");
+    // Creating Figure & Figcaption
     figure.setAttribute("id", "fig");
     body.appendChild(figure);
+
     const figcaption = document.createElement("figcaption");
     figure.appendChild(figcaption);
 }
+
+// Please end my suffering
+document.addEventListener("submit", addBook); (e) => {
+    e.preventDefault();
+    createFigure()
+
+};
