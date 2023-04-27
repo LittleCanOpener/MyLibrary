1802d6362ee259eb889ebc71ef9e37c4a044c757
LittleCanOpener <hrafn8@gmail.com>
2023-04-27 21:15:21 +0000

    test1

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
 /*jshint esversion: 6 */
 let libary = [];
-//Storing IP Address in Local Storage
+
 function Book(title, author, pages) {
     this.Title = title;
     this.Author = author;
     this.Pages = pages;
 }
-// Arry's
 function addBook () {
+    // Arry's
     let a = document.getElementById("title").value;
     let b = document.getElementById("author").value;
     let c = document.getElementById("pages").value;
     // Saving Values
     libary.push(new Book(a, b, c));
     localStorage.setItem('MyLibary', JSON.stringify(libary));
-    createText();
-}
-
-function createText() {
     const output = document.getElementById("output");
     const div = document.createElement("div");
     output.appendChild(div);
     div.setAttribute("id", "container");
-    // Displaying stored values
-    document.getElementById("container").innerHTML = localStorage.getItem('MyLibary');
     document.querySelector("form").reset();
+    output.innerHTML = localStorage.getItem('MyLibary')
 }
 
 // Main Event
@@ -32,3 +27,4 @@ document.addEventListener("submit", (ev) => {
     ev.preventDefault();
     addBook();
 });
+
