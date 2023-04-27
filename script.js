1e55d098dda82751fd2b9c920674831dc60692b4
LittleCanOpener <hrafn8@gmail.com>
2023-04-27 12:33:53 +0000

    restructured the js

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-const addClass = (selector, className, scope) => {
-    (scope || document).querySelector(selector).classList.add(className)
-};
-addClas("body", "purple");
+let libary = [];
 
-//Process Raw Data
-const processRawUserInput = () => {
-    const rawData = document.querySelector("input").value;
-    const processedData = processUserInput(rawData)
-    const libaryForm = document.querySelectorAll("libaryForm");
-    libaryForm.innerHTML = processedData;
+//Storing IP Address in Local Storage
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
-//Refine Processed Data
-const processUserInput = (inputValue) => {
-    const div = document.createElement("div");
-    div.textContent = inputValue;
-    return div.innerHTML;
+function addBook () {
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    
+	if (title !== '' && author !== '' && pages !== '' ){
+        libary.push(new Book(title, author, pages));
+		localStorage.setItem('MyLibary', JSON.stringify(libary) );
+		document.getElementById("container").innerHTML = localStorage.getItem('MyLibary');
+		document.querySelector("form").reset();
+    } 
+    else {
+        alert('Form not filled out')
+	}
 }
+
+function createText() {
+    
+    const body = document.getElementById("body");
+    const text = document.createElement("p");
+
+    body.appendChild(text);
+    text.setAttribute("id", "container");
+    
+}
+
+// Main Event
+document.addEventListener("submit", (ev) => {
+    ev.preventDefault();
+
+    createText();
+    addBook();
+});
