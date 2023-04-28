99ae4fb5c79e296295e473aad1f26fab413a8161
LittleCanOpener <hrafn8@gmail.com>
2023-04-28 11:08:12 +0000

    using classes instead

--- script.js
+++ script.js
@@ -1,30 +1,74 @@
 /*jshint esversion: 6 */
-let libary = [];
 
-function Book(title, author, pages) {
-    this.Title = title;
-    this.Author = author;
-    this.Pages = pages;
+// Book Class: Represents a Book
+class Book {
+    constructor(title, author, pages){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
 }
-function addBook () {
-    // Arry's
-    let a = document.getElementById("title").value;
-    let b = document.getElementById("author").value;
-    let c = document.getElementById("pages").value;
-    // Saving Values
-    libary.push(new Book(a, b, c));
-    localStorage.setItem('MyLibary', JSON.stringify(libary));
-    const output = document.getElementById("output");
-    const div = document.createElement("div");
-    output.appendChild(div);
-    div.setAttribute("id", "container");
-    document.querySelector("form").reset();
-    output.innerHTML = localStorage.getItem('MyLibary')
+// UI Class: Handle UI Tasks
+class UI {
+    static displayBooks(){
+        const StoredBooks = [
+            {
+                title:'Book One',
+                author:'John Doe',
+                Pages:'140'
+            },
+            {
+                title:'Book Two',
+                author:'John Glow',
+                Pages:'1650'
+            }
+        ]
+        // Get from local storage
+
+        // setting books to arry
+        const books = StoredBooks;
+        books.forEach((book) => UI.addBookToList(book));
+    }
+    static addBookToList(book) {
+        const output = document.querySelector("#output");
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.pages}</td>
+        <td><a href="#" class="delete"</a>X</td>
+        `;
+        output.appendChild(row)
+    }
+    static clearFields() {
+        document.querySelector("#title").value= '';
+        document.querySelector("#author").value= '';
+        document.querySelector("#pages").value= '';
+    }
 }
 
-// Main Event
-document.addEventListener("submit", (ev) => {
-    ev.preventDefault();
-    addBook();
+// Store Class: Handles Storage
+
+
+// Events Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Event Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    
+    e.preventDefault();
+    // Get Values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+
+    // Instatiate Book
+    const book = new Book(title, author,pages)
+    // Adding Book To UI
+    UI.addBookToList(book);
+    UI.clearFields();
+    console.log(book)
+
 });
-
+// Event Remove a Book
